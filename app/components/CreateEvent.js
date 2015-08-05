80eff05dd990dff5125c3e66d7dee86ec1c223e5
Ryan Herlihy <ryan.herlihy@sonian.net>
2015-08-05 15:54:33 -0400

    cleaned up commented out code

--- app/components/CreateEvent.js
+++ app/components/CreateEvent.js
@@ -1,18 +1,14 @@
 'use strict';
 
 import React from 'react';
-import Modal from 'react-modal';
 import TestWindow from './TestWindow';
+
 import PinnStore from '../stores/PinnStore';
 import PinnActions from '../actions/PinnActions';
 
 const {
   Component
 } = React;
-
-// let appElement = document.getElementById('info-window');
-// Modal.setAppElement(appElement);
-// Modal.injectCSS();
 
 class CreateEvent extends Component {
   constructor(props) {
@@ -36,44 +32,6 @@ class CreateEvent extends Component {
   }
 
   render() {
-    // console.log(this.props.modalOpen);
-    // return (
-    //   <div className='modal fade' id='create-event'>
-    //     <div className='modal-dialog'>
-    //       <div className='modal-content'>
-    //         <div className='modal-header lead'>
-    //           Create New Event
-    //         </div>
-    //         <div className='modal-body'>
-    //           <div className='container-fluid'>
-    //             <div className='row'>
-    //               <form>
-    //                 <div className='form-group'>
-    //                   <label forHTML='new-event-name'>Event Name</label>
-    //                   <input
-    //                     type='text'
-    //                     className='form-control'
-    //                     id='new-event-name'
-    //                     placeholder='Event Name' />
-    //                 </div>
-    //                 <div className='form-group'>
-    //                   <label forHTML='new-event-desc'>Event Description</label>
-    //                   <textarea
-    //                     className='form-control'
-    //                     id='new-event-desc'
-    //                     rows='5'
-    //                     placeholder='Event Description' >
-    //                   </textarea>
-    //                 </div>
-    //                 <button type='button' className='btn btn-danger' onClick={this.handleSubmit}>Create Event</button>
-    //               </form>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // );
     let content =
       <div className='container-fluid'>
         <div className='row'>
