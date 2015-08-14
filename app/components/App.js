36bf9a2805d5be33bb5e53db19da9d25f3e285fc
Ryan Herlihy <ryan.herlihy@sonian.net>
2015-08-14 00:05:38 -0400

    added background styling

--- app/components/App.js
+++ app/components/App.js
@@ -4,8 +4,6 @@ import React from 'react';
 import { RouteHandler } from 'react-router';
 import Map from './Map';
 
-import PinnStore from '../stores/PinnStore';
-
 const {
   Component
 } = React;
@@ -13,15 +11,19 @@ const {
 class App extends Component {
   constructor() {
     super();
-
-    this.state = PinnStore.getState();
   }
 
   render() {
     return (
       <div id='app'>
+        <header>
+          <img src='../../app/images/pinnditLogo.png' />
+        </header>
         <Map />
         <RouteHandler />
+        <footer align='center'>
+          Pinnd.It &copy; Copyright 2015
+        </footer>
       </div>
     );
   }
